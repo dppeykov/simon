c9aeff5b093ff04202e2c90bb2fe66a679a1e0a4
Damyan Peykov <dppeykov@gmail.com>
2019-01-24 16:15:45 +0100

    Add files via upload

--- game.js
+++ game.js
@@ -19,8 +19,8 @@ let nextSequence = () => {
 
     let randomNumber = Math.floor(Math.random()*4);
     let randomChosenColour = buttonColours[randomNumber];  
-    $("#" + randomChosenColour).addClass("flash");
     playSound(randomChosenColour);
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
     gamePattern.push(randomChosenColour);
 
 }
